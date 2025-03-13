431a3c4f0447446fcbe49c20277f9f4437c56806
Dhruv <sanghvidhruv20@gmail.com>
2025-03-13 19:33:11 +0530

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import React from "react";
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 const App = () => {
   return (
     <div>
