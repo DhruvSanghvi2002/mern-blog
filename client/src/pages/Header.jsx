1c8317fc4f3287c4d9b64da1c68688ae7894c17d
Dhruv <sanghvidhruv20@gmail.com>
2025-03-13 19:50:37 +0530

    create and run the server

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -35,7 +35,9 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">SignIn</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>
+            SignIn
+          </Button>
         </Link>
         <Navbar.Toggle />
       </div>
